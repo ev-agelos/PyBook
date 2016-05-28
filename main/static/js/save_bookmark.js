723e0a10dbee4a3394bf81e7b0674e62f2e54cde
Evagelos Theodoridis <evagelos.theo@gmail.com>
2016-05-28 14:54:34 +0200

    The save link is the second element after edit link

--- main/static/js/save_bookmark.js
+++ main/static/js/save_bookmark.js
@@ -2,14 +2,14 @@ function save(id, bookmark_id)
 {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function(){
-        var element = document.getElementById(id).getElementsByTagName('li')[0].getElementsByTagName('a')[0];
+        var element = document.getElementById(id).getElementsByTagName('li')[1].getElementsByTagName('a')[0];
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
             if (xmlHttp.responseText == 'Saved'){
                 var message = 'unsave';
             }else if (xmlHttp.responseText == 'Unsaved'){
                 var message = 'save';
             }
-            element.innerHTML=message;
+            element.innerHTML='<small>' + message + '<small>';
         }
     }
     xmlHttp.open("GET", '/bookmarks/save?bookmark_id=' + bookmark_id, true);
