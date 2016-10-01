b5dd362c1ed7ab6d33d888e1c7b0f0eba4d80c90
ev-agelos <evagelos.theo@gmail.com>
2016-10-01 14:44:15 +0200

    Remove app context dependency

--- bookmarks/auth/token.py
+++ bookmarks/auth/token.py
@@ -1,18 +1,17 @@
 """Helper functions for handling tokens."""
 
-from flask import current_app as app
 from itsdangerous import URLSafeTimedSerializer
 
 
-def generate_user_token(email):
+def generate(email, secret_key):
     """Generate token with the email address."""
-    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    serializer = URLSafeTimedSerializer(secret_key)
     return serializer.dumps(email)
 
 
-def confirm_token(token, expiration=3600):
+def confirm(token, secret_key, expiration=3600):
     """Confirm a given token if it is valid."""
-    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    serializer = URLSafeTimedSerializer(secret_key)
     try:
         email = serializer.loads(token, max_age=expiration)
     except Exception:
