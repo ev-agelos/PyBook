c435de3211c1551cf74ecb461744417ab52870c1
ev-agelos <evagelos.theo@gmail.com>
2016-12-31 13:46:03 +0100

    dont block when sending emails

--- bookmarks/users/views/utils.py
+++ bookmarks/users/views/utils.py
@@ -1,19 +1,26 @@
 """Helper utilities."""
 
+from threading import Thread
 
 from flask import current_app
 import requests
 
 
+def _send_async_email(app, payload):
+    """Make the request to send the email asynchronously."""
+    requests.post(app.config['MAIL_DOMAIN'],
+                  auth=('api', app.config['MAIL_KEY']), data=payload)
+
+
 def send_email(subject, receiver, text):
     """Send email if configuration was set during app initiation."""
-    mailgun_keys = ('MAILGUN_SENDER', 'MAILGUN_DOMAIN', 'MAILGUN_KEY')
+    mailgun_keys = ('MAIL_DEFAULT_SENDER', 'MAIL_DOMAIN', 'MAIL_KEY')
     if not any(current_app.config.get(key) for key in mailgun_keys):
         return False
 
     payload = {'subject': subject, 'text': text, 'to': [receiver],
-               'from': current_app.config['MAILGUN_SENDER']}
-    response = requests.post(current_app.config['MAILGUN_DOMAIN'],
-                             auth=('api', current_app.config['MAILGUN_KEY']),
-                             data=payload)
-    return response.status_code == 200
+               'from': current_app.config['MAIL_DEFAULT_SENDER']}
+    app = current_app._get_current_object()
+    thr = Thread(target=_send_async_email, args=[app, payload])
+    thr.start()
+    return True
