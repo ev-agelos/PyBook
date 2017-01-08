a552647803d4b8a385c92811d508b852457799b3
ev-agelos <evagelos.theo@gmail.com>
2017-01-08 22:15:15 +0100

    added tests

---
+++ tests/test_bookmarks_utils.py
@@ -0,0 +1,15 @@
+import pytest
+
+from bookmarks.utils import send_email
+
+
+def test_sending_email_without_credentials(app):
+    with app.app_context():
+        assert not send_email('a', 'b', 'c')
+
+
+def test_sending_email_with_credentials(app):
+    with app.app_context():
+        app.config.update(MAIL_DEFAULT_SENDER='a', MAIL_DOMAIN='b',
+                          MAIL_KEY='c')
+        assert send_email('a', 'b', 'c')
