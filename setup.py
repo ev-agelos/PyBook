0fb74f1a116fa0af3cdb9df0ef15360b1832e7a6
ev-agelos <evagelos.theo@gmail.com>
2017-01-10 23:00:50 +0100

    added setup.py, removed __init__ from tests as PyTest recommends

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='PyBook',
+      version='0.1',
+      description='Flask webapp/api to gather python related content',
+      url='https://gitlab.com/evagelos/PyBook',
+      author='Evagelos Theodoridis',
+      author_email='evagelos.theo@google.com',
+      license='MIT'
+)
