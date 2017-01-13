e5e7b0e8c61109b27fd83b05206b6f963aeb2e1f
ev-agelos <evagelos.theo@gmail.com>
2017-01-13 21:00:32 +0100

    delete user's favourites when deleting user

--- bookmarks/users/models.py
+++ bookmarks/users/models.py
@@ -41,7 +41,8 @@ class User(db.Model, UserMixin):
 
     bookmarks = db.relationship(Bookmark, backref='user',
                                 cascade='all, delete-orphan', lazy='dynamic')
-    favourites = db.relationship(Favourite, backref='user', lazy='dynamic')
+    favourites = db.relationship(Favourite, backref='user', lazy='dynamic',
+                                 cascade='all')
     votes = db.relationship(Vote, backref='user', lazy='dynamic',
                             cascade='all, delete-orphan')
     subscribed = db.relationship(
