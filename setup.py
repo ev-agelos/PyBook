a53f0b442e0bcca28eaf786805620fa3bbd6df06
ev-agelos <evagelos.theo@gmail.com>
2017-01-14 09:42:48 +0100

    read requirements form setup.py
    closes #16

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup
 
+
+with open('requirements/dev.txt') as f:
+    install_requires = f.read().splitlines()
+
+
 setup(name='PyBook',
       version='0.1',
+      install_requires=install_requires,
+      test_require=['pytest'],
       description='Flask webapp/api to gather python related content',
       url='https://gitlab.com/evagelos/PyBook',
       author='Evagelos Theodoridis',
