7dce9249329ac035a867c643ddb6b0d3e7776722
ev-agelos <evagelos.theo@gmail.com>
2020-03-17 09:07:44 +0100

    start tracking migrations, update gitignore

---
+++ migrations/versions/fed4c68c35d7_initial_migration.py
@@ -0,0 +1,24 @@
+"""Initial migration
+
+Revision ID: fed4c68c35d7
+Revises:
+Create Date: 2020-03-17 08:53:42.742028
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fed4c68c35d7'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
