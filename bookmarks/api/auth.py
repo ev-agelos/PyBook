3a0841568126df191e1585cb6f52a3991829a7d8
ev-agelos <evagelos.theo@gmail.com>
2020-06-16 09:57:42 +0200

    request token through flask-login

--- bookmarks/api/auth.py
+++ bookmarks/api/auth.py
@@ -3,6 +3,7 @@
 
 from flask import Blueprint, g, jsonify
 from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
+from flask_login import login_required
 
 from bookmarks import csrf
 from bookmarks.users.models import User
@@ -35,7 +36,7 @@ def verify_token(token):
 
 @auth_api.route('/api/v1/auth/request-token', methods=['POST'])
 @csrf.exempt
-@basic_auth.login_required
+@login_required
 def request_token():
     """Return new token for the user."""
     return jsonify({'token': g.user.generate_auth_token()}), 200
