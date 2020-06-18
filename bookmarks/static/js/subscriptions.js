4d0a574e698cd48145f49f099cd6ef4085295239
ev-agelos <evagelos.theo@gmail.com>
2020-06-18 12:35:08 +0200

    make available to (un)subscribe from the main page

--- bookmarks/static/js/subscriptions.js
+++ bookmarks/static/js/subscriptions.js
@@ -1,11 +1,27 @@
-document.addEventListener('DOMContentLoaded', function(){
-    $('#subscriptionCheckbox').on('click', subscription);
-});
+function checkSubscription() {
+    let modal = $("#subscribeModal");
+    let checkbox = modal.find("#subscriptionCheckbox")[0];
+    checkbox.checked = false; // reset before the check
 
+    fetch('/api/v1/subscriptions/', {
+        headers: {'Content-Type': 'application/json'},
+    })
+        .then(response => response.json())
+        .then(data => {
+            for (let object of data){
+                if (object["user"].endsWith('/' + this.dataset.userId)){
+                    checkbox.checked = true;
+                    break;
+                }
+            }
+            checkbox.dataset.userId = this.dataset.userId;
+            checkbox.dataset.username = this.dataset.username;
+            M.Modal.getInstance(modal).open();
+        })
+}
 
-subscription = function(){
-    let username = this.dataset.userUsername;
-
+function subscription(){
+    let username = this.dataset.username;
     if (this.checked){
         fetch('/api/v1/subscriptions/', {
             method: 'POST',
@@ -15,6 +31,7 @@ subscription = function(){
             if (response.status == 204){
                 return {message: 'Subscribed to ' + username}
             }else {
+                this.checked = false; // reset state
                 return response.json()
             }
         }).then(data => M.toast({html: data['message']}))
@@ -26,8 +43,18 @@ subscription = function(){
             if (response.status == 204){
                 return {message: 'Unsubscribed from ' + username}
             }else {
+                this.checked = true; // reset state
                 return response.json()
             }
         }).then(data => M.toast({html: data['message']}))
     }
 };
+
+document.querySelectorAll("#usernameLink").forEach(function(item) {
+    item.addEventListener('click', checkSubscription);
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+    $('#subscriptionCheckbox').on('click', subscription);
+});
+
