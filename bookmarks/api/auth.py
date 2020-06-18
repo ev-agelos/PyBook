27ddef22f6d36507eb8fdcff78b3aab2925e1fd2
ev-agelos <evagelos.theo@gmail.com>
2020-06-18 12:47:46 +0200

    remove flask-httpauth

--- bookmarks/api/auth.py
+++ bookmarks/api/auth.py
@@ -2,7 +2,6 @@
 
 
 from flask import Blueprint, g, jsonify
-from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 from flask_login import login_required
 
 from bookmarks import csrf
@@ -10,28 +9,6 @@ from bookmarks.users.models import User
 
 
 auth_api = Blueprint('api', __name__)
-
-basic_auth = HTTPBasicAuth()
-token_auth = HTTPTokenAuth(scheme='Bearer')
-
-
-@basic_auth.verify_password
-def verify_password(email, password):
-    """Verify user by email address and password."""
-    g.user = User.query.filter_by(email=email).scalar()
-    return g.user is not None and g.user.is_password_correct(password)
-
-
-
-@token_auth.verify_token
-def verify_token(token):
-    """Verify user by token."""
-    data = User.verify_auth_token(token)
-    user = User.query.get(data['id']) if data else None
-    if user:
-        g.user = user
-        return True
-    return False
 
 
 @auth_api.route('/api/v1/auth/request-token', methods=['POST'])
