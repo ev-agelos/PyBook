9f63eb3a6c1f0db187ffee44603641bbbab3f11b
ev-agelos <evagelos.theo@gmail.com>
2020-07-01 10:35:03 +0200

    add a logout endpoint under flask-smorest

--- bookmarks/api/auth.py
+++ bookmarks/api/auth.py
@@ -2,9 +2,9 @@
 
 
 from flask import Blueprint, g, jsonify
-from flask_login import login_required
+from flask_login import login_required, logout_user
 
-from bookmarks import csrf
+from bookmarks import csrf, db
 from bookmarks.users.models import User
 
 
@@ -17,3 +17,15 @@ auth_api = Blueprint('api', __name__)
 def request_token():
     """Return new token for the user."""
     return jsonify({'token': g.user.generate_auth_token()}), 200
+
+
+@auth_api.route('/api/v1/auth/logout')
+@csrf.exempt
+@login_required
+def logout():
+    """Logout a user."""
+    g.user.auth_token = ''
+    g.user.authenticated = False
+    logout_user()
+    db.session.commit()
+    return {}, 204
