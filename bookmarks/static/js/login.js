694f216ce89dd425c2d007c5d01c7bdf6fcc17d4
ev-agelos <evagelos.theo@gmail.com>
2020-07-06 21:05:58 +0200

    remove js files

--- bookmarks/static/js/login.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    $('#submitButton').on('click', function(event){
-        document.getElementById('card').style.opacity = 0.3;
-        document.getElementById('loadingCircle').className += ' active';
-    });
-})
