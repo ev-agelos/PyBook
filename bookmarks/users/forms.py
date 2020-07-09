11cd11c35d0c79bc50d7106794ad2aba5b901159
ev-agelos <evagelos.theo@gmail.com>
2020-07-09 10:46:09 +0200

    remove unused forms

--- bookmarks/users/forms.py
+++ bookmarks/users/forms.py
@@ -1,45 +1,9 @@
 """Forms for auth package."""
 
 
-from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import PasswordField, StringField, BooleanField
-from wtforms.fields.html5 import EmailField
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField
 from wtforms.validators import InputRequired, Email, EqualTo, Length
-
-
-class LoginForm(FlaskForm):
-    """Login form for the application."""
-
-    email = EmailField('Email', validators=[InputRequired(), Email()])
-    password = PasswordField('Password', validators=[InputRequired()])
-    remember_me = BooleanField('Remember me')
-
-
-class RegistrationForm(FlaskForm):
-    """Registration form for new users."""
-
-    username = StringField('Username', validators=[InputRequired(),
-                                                   Length(min=3, max=25)])
-    email = StringField('Email', validators=[InputRequired(),
-                                             Email(message=None),
-                                             Length(min=6, max=40)])
-    password = PasswordField('Password', validators=[InputRequired(),
-                                                     Length(min=6, max=25)])
-    confirm_password = PasswordField(
-        'Repeat password',
-        validators=[InputRequired(), EqualTo('password',
-                                             message='Passwords must match.')])
-    recaptcha = RecaptchaField()
-
-
-class ProfileForm(FlaskForm):
-    """Form with user's details."""
-
-    username = StringField('Username', validators=[InputRequired(),
-                                                   Length(min=3, max=25)])
-    email = StringField('Email', validators=[InputRequired(),
-                                             Email(message=None),
-                                             Length(min=6, max=40)])
 
 
 class ChangePasswordForm(FlaskForm):
