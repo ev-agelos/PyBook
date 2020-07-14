a68f1ffa046f18cb46dcdba2bc0613dfb7ddc48f
ev-agelos <evagelos.theo@gmail.com>
2020-07-14 13:49:16 +0200

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements/dev.txt') as f:
 
 
 setup(name='PyBook',
-      version='1.0.0',
+      version='1.0.1',
       python_requires='==3.8.*',
       install_requires=install_requires,
       test_require=['pytest', 'pytest-xdist'],
