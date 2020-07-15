f6c4e577e40635dba9817994bc4818fc4db22f78
ev-agelos <evagelos.theo@gmail.com>
2020-07-15 11:05:31 +0200

    fix page title

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,12 @@
 const path = require('path');
 
 module.exports = {
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            title: 'PyBook'
+        }
+    },
     devServer: {
         proxy: {
             '^/': {
